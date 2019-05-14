8137fdcb3d14f24bfd11c3f37da2d79200e3ea23
Cristiano <cristiano@cristianocosta.me>
2019-05-14 16:40:17 -0300

    Fork of react-leaflet

---
+++ example/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: [path.join(__dirname, 'index.js')],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: __dirname,
+    port: 8000,
+  },
+}
